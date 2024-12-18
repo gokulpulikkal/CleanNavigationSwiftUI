d8a9954130664bb99b9b33933758f92429d4646a
Gokul <gokulplkl@gmail.com>
2024-12-18 12:32:48 -0500

    Initial Commit

---
+++ CleanNavigationStudy/CleanNavigationStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanNavigationStudyApp.swift
+//  CleanNavigationStudy
+//
+//  Created by Gokul P on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanNavigationStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanNavigationStudyTests/CleanNavigationStudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanNavigationStudyTests.swift
+//  CleanNavigationStudyTests
+//
+//  Created by Gokul P on 12/18/24.
+//
+
+import Testing
+@testable import CleanNavigationStudy
+
+struct CleanNavigationStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
